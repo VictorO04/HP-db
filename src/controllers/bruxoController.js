a62e4bae6b465cddf201ff1d9b242d176e687a94
Victor <victorfoliveira0407@gmail.com>
2025-10-27 09:05:17 -0300

    cria post e update

--- src/controllers/bruxoController.js
+++ src/controllers/bruxoController.js
@@ -53,3 +53,72 @@ export const listarUm = async (req, res) => {
         });
     }
 }
+
+export const criar = async (req, res) => {
+    try {
+      const { nome, casa, patrono, varinha, anoMatricula } = req.body;
+  
+      const dado = req.body;
+  
+      // Validação automática de campos obrigatórios
+      const camposObrigatorios = ["nome", "casa", "varinha", "anoMatricula"];
+  
+      const faltando = camposObrigatorios.filter((campo) => !dado[campo]);
+  
+      if (faltando.length > 0) {
+        return res.status(400).json({
+          erro: `Os seguintes campos são obrigatórios: ${faltando.join(", ")}.`,
+        });
+      }
+  
+      // Validar se a casa é válida
+      const casasValidas = ["Grifinória", "Sonserina", "Corvinal", "Lufa-Lufa"];
+      if (!casasValidas.includes(casa)) {
+        return res.status(400).json({
+          erro: "Casa inválida! O Chapéu Seletor só reconhece as 4 casas",
+          casasValidas,
+        });
+      }
+  
+      //Crio o bruxo usando model
+      const novoBruxo = await BruxoModel.criar(req.body);
+  
+      res.status(201).json({
+        mensagem: "Bruxo criado com sucesso!",
+        bruxo: novoBruxo,
+      });
+    } catch (error) {
+      res.status(500).json({
+        erro: "Erro ao criar bruxo",
+        detalhes: error.message,
+      });
+    }
+  };
+
+export const deletar = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        
+        const bruxoExiste = await BruxoModel.encontreUm(id);
+
+        if (!bruxoExiste) {
+            return res.status(404).json({
+                erro: 'Bruxo não encontrado com esse id',
+                id: id
+            });
+        }
+
+        await BruxoModel.deletar(id);
+
+        res.status(200).json({
+            mensagem: 'Bruxo apagado com sucesso!',
+            bruxoRemovido: bruxoExiste
+        });
+    } catch (error) {
+        res.status(500).json({
+            erro: 'Erro ao apagar bruxo',
+            detalhes: error.message,
+            status: 500
+        });
+    }
+}
--- src/models/bruxoModel.js
+++ src/models/bruxoModel.js
@@ -1,14 +1,7 @@
-// Consulta para o banco de dados
-
-//importar prisma client
-
 import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
-// Criar exportando a variável -> findALL que vai ser o SELECT * FROM bruxos;
-
 export const encontreTodos = async () => {
-    // SELECT * FROM bruxos;
     return await prisma.bruxo.findMany({
         orderBy: {nome: 'asc'}
     });
@@ -17,5 +10,23 @@ export const encontreTodos = async () => {
 export const encontreUm = async (id) => {
     return await prisma.bruxo.findUnique({
         where: {id: Number(id)}
+    });
+}
+
+export const criar = async (dado) => {
+    return await prisma.bruxo.create({
+        data: {
+            nome: dado.nome,
+            casa: dado.casa,
+            patrono: dado.patrono,
+            varinha: dado.varinha,
+            anoMatricula: dado.anoMatricula
+        }
+    })
+}
+
+export const deletar = async (id) => {
+    return await prisma.bruxo.delete({
+        where: { id: Number(id) }
     });
 }
